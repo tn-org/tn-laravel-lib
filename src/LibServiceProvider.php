d06df885325eb86a7fae9eac38ef7e296e34aa2c
tnlake <tn.tech@icloud.com>
2025-09-23 13:16:32 +0900

    prettier導入

--- src/LibServiceProvider.php
+++ src/LibServiceProvider.php
@@ -9,14 +9,17 @@ class LibServiceProvider extends ServiceProvider
     public function register(): void
     {
         // パッケージ内のデフォルト設定を取り込む（アプリ側で上書き可）
-        $this->mergeConfigFrom(__DIR__ . '/../config/tn-lib.php', 'tn-lib');
+        $this->mergeConfigFrom(__DIR__ . "/../config/tn-lib.php", "tn-lib");
     }
 
     public function boot(): void
     {
         // アプリ側に設定ファイルを publish できるようにする
-        $this->publishes([
-            __DIR__ . '/../config/tn-lib.php' => $this->app->configPath('tn-lib.php'),
-        ], 'tn-lib-config');
+        $this->publishes(
+            [
+                __DIR__ . "/../config/tn-lib.php" => $this->app->configPath("tn-lib.php")
+            ],
+            "tn-lib-config"
+        );
     }
 }
